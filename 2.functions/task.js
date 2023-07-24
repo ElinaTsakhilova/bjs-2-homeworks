aed8991db14644114704e4d184473fbc9473703e
ElinaTsakhilova <lina_iz_berlina@mail.ru>
2023-07-24 14:35:57 +0300

    The second task is ready

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -62,7 +62,6 @@ function makeWork (arrOfArr, func) {
   let maxWorkerResult = 0;
   for(let i = 0; i < arrOfArr.length; i++) {
     const result = func(...arrOfArr[i]);
-
     if (result > maxWorkerResult) {
       maxWorkerResult = result;
     }
