26f5127b2154f76e0db93fb7b6abcb7337138204
ElinaTsakhilova <lina_iz_berlina@mail.ru>
2023-07-29 22:44:19 +0300

    The third task is ready

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
+// Task 1
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  return arr1.every((element, i) => element === arr2[i])
 }
 
+//Task 2
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users.filter(user => gender === user.gender).map(user => user.age).reduce((acc, age, index, arr) => acc + age / arr.length, 0)
+    return result;
 }
