dd4f0869c1d8b6193af4b405910c1533428be995
ElinaTsakhilova <lina_iz_berlina@mail.ru>
2023-08-09 22:18:28 +0300

    The fourth task is ready

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,42 @@
 function Student(name, gender, age) {
-  
+
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
+    new Student ();
+    new Student ();
+    new Student ();
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
   
+   if (this.marks) {
+    this.marks.push(...marks)
+  } 
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (this.marks === undefined) {
+    return 0;
+  } else {
+    return this.marks.reduce ((acc, curr) => acc + curr / this.marks.length, 0);
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+	delete this.marks;
+	this.excluded=reason;
 }
+
+
+/*Student.prototype.addMarks = function(...marks) {
+	if (this.marks) {
+	this.marks.push(...marks);
+	}
+}*/
